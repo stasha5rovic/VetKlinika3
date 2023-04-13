3a58a214cece4bc0593c9db0b7f52097f516cf1e
dernhelm2040 <altorishome@gmail.com>
2023-04-13 12:22:13 +0200

    zapoceti blejdovi

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -4,9 +4,9 @@
             <div class="row">
                 <div class="col-12 text-center pt-5">
 
-                <h1 class="display-one mt-10">{{ config('app.name') }}</h1>
+                <h1 class="display-one mt-5">{{ config('app.name') }}</h1>
                 <br><br><br><br>
-                <p><b> Dobro došli u našu Vet Kliniku!</b> </p>
+               
                 <br>
                 <a href="/index" class="btn btn-outline-primary">Ulogujte se</a>
                 </div>
